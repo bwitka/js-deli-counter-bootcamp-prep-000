ea546820fa3c50c2bff5777ecfed3b6a6c95b4a4
BEAU WITKA <bwitka@gmail.com>
2018-12-19 02:17:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(line, name) {
   let i = 0;
   line.push(name);
-    return `Welcome, ${name}. You are number ${[i] + 1} in line.`;
+    return `Welcome, ${name}. You are number ${i + 1} in line.`;
 }
 
 function nowServing(line) {
